de93ee28fc7adf4fdc24c9e1fb9b1c791762422c
Ilya Fedosov <leotards2015@gmail.com>
2021-06-23 15:18:25 +0300

    solved examples

--- src/main/java/lect1/additionaltasks/Main.java
+++ src/main/java/lect1/additionaltasks/Main.java
@@ -3,7 +3,7 @@ package lect1.additionaltasks;
 public class Main {
     public static void main(String[] args)
     {
-
+        System.out.println(getNumber(167, 135));
     }
 
     //TODO: Вместо void указать правильный тип данных (Сделано на лекции)
@@ -19,31 +19,51 @@ public class Main {
         //TODO: Реализовать алгоритм, который возвращает true, если входной параметр number чётный
         // В противном случае вернуть false
         // PS: Чётность определяется результатом получения остатка от деления на 2. Как получить остаток от деления - гуглите.
-        return false;
+        return number % 2 == 0;
     }
 
     public static Integer getNumber(Integer number1, Integer number2)
     {
         //TODO: Реализовать алгоритм, который определяет, какое из чисел (number1 или number2) ближе к 150 и возвращает
         // его в качестве результата
-        return number1;
+        if((150 - number1) < (150 - number2))
+            return number1;
+        else
+            return number2;
     }
 
     //TODO: Задание "Со звёздочкой"
-    public static Integer getSecondNumber(Integer number1, Integer number2)
+    public static Integer getSecondNumber(Integer number1)
     {
         //TODO: Реализовать алгоритм, который принимает трехзначное число и возвращает его вторую цифру
         // Например: Дано 658, алгоритм вернул 5
         // PS: Соответствующая цифра также определяется операцией нахождения остатка от деления
-        return 999;
+        int res = number1 % 100;
+        res = res / 10;
+        return res;
     }
 
     //TODO: Задание "Со звёздочкой"
-    public static float solve(Integer a, Integer b, Integer c)
+    public static double solve(Integer a, Integer b, Integer c)
     {
         //TODO: Реализовать алгоритм, который находит сумму корней квадратного уравнения ax*x + bx + x = 0
         // Если корней не существует, необходимо вернуть -1
         // PS: Решение находится с помощью теоремы Виета или через нахождение дискриминанта
-        return 999f;
+        double discriminant = (b*b) - (4 * a * c);
+
+        if (discriminant > 0)
+        {
+            double x1 = (-b-Math.sqrt(discriminant))/(2*a);
+            double x2 = (-b+Math.sqrt(discriminant))/(2*a);
+            return x1 + x2;
+        }
+
+        if (discriminant == 0)
+        {
+            double x1 = (-b)/(2*a);
+            return x1;
+        }
+        return -1;
+
     }
 }
--- src/main/java/lect2/additionaltasks/Main.java
+++ src/main/java/lect2/additionaltasks/Main.java
@@ -3,7 +3,7 @@ package lect2.additionaltasks;
 public class Main {
     public static void main(String[] args)
     {
-
+        printNums();
     }
 
     // TODO: Вместо void указать правильный тип данных (если это необходимо)
@@ -24,15 +24,45 @@ public class Main {
         // 3) Если текущий элемент == 9, то необходимо прервать выполнение цикла.
         // 4) Если не выполнено ни одно из условий, вывести в консоль строку : "Не знаю что делать"
         // ВАЖНО: в реализации должны быть использованы switch, break, continue, default
+        for(int i = 0; i < arr.length; i++)
+        {
+            switch (arr[i])
+            {
+                case 1,2,3:
+                    System.out.println(arr[i]);
+                    break;
+                case 4,5,6:
+                    continue;
+                case 9:
+                    return;
+                default:
+                    System.out.println("Не знаю что делать");
+            }
+        }
     }
 
     public static void fib(Integer n)
     {
         //TODO: Реализовать алгоритм, который выводит первые n чисел ряда Фибоначчи(1, 1, 2, 3, 5...)
+        int[] arr = new int[n];
+        arr[0] = 1;
+        arr[1] = 1;
+        for (int i = 2; i < arr.length; ++i) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+
+        for (int i = 0; i < arr.length; ++i) {
+            System.out.println(arr[i]);
+        }
     }
 
     public static void fact(Integer n)
     {
         //TODO: Реализовать алгоритм, который выводит факториал входного параметра n
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        System.out.println(result);
     }
 }
